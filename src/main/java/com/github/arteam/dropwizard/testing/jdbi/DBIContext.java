bb7d6c57a61d71c3b4c393013f20be4de5b4110d
Artem Prigoda <aprigoda@bcc.ru>
2015-01-22 21:02:08 +0300

    Start a database before test class
    We start HSQLDB and crate correct DBI and instances
    (same as in dropwizard-jdbi module)

---
+++ src/main/java/com/github/arteam/dropwizard/testing/jdbi/DBIContext.java
@@ -0,0 +1,62 @@
+package com.github.arteam.dropwizard.testing.jdbi;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import io.dropwizard.jdbi.ImmutableListContainerFactory;
+import io.dropwizard.jdbi.ImmutableSetContainerFactory;
+import io.dropwizard.jdbi.NamePrependingStatementRewriter;
+import io.dropwizard.jdbi.OptionalContainerFactory;
+import io.dropwizard.jdbi.args.JodaDateTimeArgumentFactory;
+import io.dropwizard.jdbi.args.JodaDateTimeMapper;
+import io.dropwizard.jdbi.args.OptionalArgumentFactory;
+import io.dropwizard.jdbi.logging.LogbackLog;
+import org.skife.jdbi.v2.ColonPrefixNamedParamStatementRewriter;
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Date: 1/18/15
+ * Time: 3:11 PM
+ *
+ * @author Artem Prigoda
+ */
+public class DBIContext {
+
+    public static final Logger LOG = (Logger) LoggerFactory.getLogger(DBI.class);
+
+    private DBI dbi;
+    private Handle handle;
+
+    public DBIContext() {
+        dbi = createDBI();
+        handle = dbi.open();
+    }
+
+    public DBI createDBI() {
+        DBI dbi = new DBI("jdbc:hsqldb:mem:DbTest-" + System.currentTimeMillis(), "sa", "");
+
+        dbi.setSQLLog(new LogbackLog(LOG, Level.INFO));
+        dbi.setStatementRewriter(new NamePrependingStatementRewriter(new ColonPrefixNamedParamStatementRewriter()));
+        dbi.registerArgumentFactory(new OptionalArgumentFactory(null));
+        dbi.registerContainerFactory(new ImmutableListContainerFactory());
+        dbi.registerContainerFactory(new ImmutableSetContainerFactory());
+        dbi.registerContainerFactory(new OptionalContainerFactory());
+        dbi.registerArgumentFactory(new JodaDateTimeArgumentFactory());
+        dbi.registerMapper(new JodaDateTimeMapper());
+        return dbi;
+    }
+
+    public void close() {
+        handle.close();
+    }
+
+    public DBI getDbi() {
+        return dbi;
+    }
+
+    public Handle getHandle() {
+        return handle;
+    }
+}
+
