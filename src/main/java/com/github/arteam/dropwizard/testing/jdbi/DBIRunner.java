6ed3e2326b10564cc63c71ec5cd9212517e1e3de
Artem Prigoda <aprigoda@bcc.ru>
2015-01-25 23:37:23 +0300

    Refactor injecting test artifacts to separate methods
    It's too big to process in one method

--- src/main/java/com/github/arteam/dropwizard/testing/jdbi/DBIRunner.java
+++ src/main/java/com/github/arteam/dropwizard/testing/jdbi/DBIRunner.java
@@ -37,7 +37,6 @@ public class DBIRunner extends BlockJUnit4ClassRunner {
     protected Object createTest() throws Exception {
         Object test = super.createTest();
         injectTestObjects(test);
-
         return test;
     }
 
@@ -53,86 +52,96 @@ public class DBIRunner extends BlockJUnit4ClassRunner {
             }
             for (Annotation annotation : annotations) {
                 if (annotation.annotationType().equals(DBIHandle.class)) {
-                    if (!field.getGenericType().equals(Handle.class)) {
-                        throw new IllegalArgumentException("Unable inject a DBI handle to a " +
-                                "field with type " + field.getGenericType());
-                    }
-                    if (Modifier.isStatic(field.getModifiers())) {
-                        throw new IllegalArgumentException("Unable inject a DBI Handle to a static field");
-                    }
-                    field.setAccessible(true);
-                    field.set(test, handle);
-                }
-                if (annotation.annotationType().equals(DBIInstance.class)) {
-                    if (!field.getGenericType().equals(DBI.class)) {
-                        throw new IllegalArgumentException("Unable inject a DBI instance to " +
-                                "a field with type " + field.getGenericType());
-                    }
-                    if (Modifier.isStatic(field.getModifiers())) {
-                        throw new IllegalArgumentException("Unable inject a DBI instance to a static field");
-                    }
-                    field.setAccessible(true);
-                    field.set(test, dbi);
-                }
-                if (annotation.annotationType().equals(TestedDBIDao.class)) {
-                    if (Modifier.isStatic(field.getModifiers())) {
-                        throw new IllegalArgumentException("Unable inject a DBI DAO to a static field");
-                    }
-
-                    Object dbiDao;
-
-                    Constructor<?> defaultConstructor = null;
-                    Constructor<?> constructorWithParameters = null;
-                    Constructor<?>[] constructors = field.getType().getDeclaredConstructors();
-                    for (Constructor<?> constructor : constructors) {
-                        Class<?>[] parameterTypes = constructor.getParameterTypes();
-                        if (parameterTypes.length == 1 && parameterTypes[0].equals(DBI.class)) {
-                            constructorWithParameters = constructor;
-                        } else if (parameterTypes.length == 0) {
-                            defaultConstructor = constructor;
-                        }
-                    }
-                    if (constructorWithParameters != null) {
-                        constructorWithParameters.setAccessible(true);
-                        try {
-                            dbiDao = constructorWithParameters.newInstance(dbi);
-                        } catch (Exception e) {
-                            throw new RuntimeException("Unable to create an instance of class '"
-                                    + field.getClass() + "'", e);
-                        }
-                    } else if (defaultConstructor != null) {
-                        defaultConstructor.setAccessible(true);
-                        try {
-                            dbiDao = defaultConstructor.newInstance();
-                        } catch (Exception e) {
-                            throw new RuntimeException("Unable to create an instance of class '"
-                                    + field.getClass() + "'", e);
-                        }
-                        Field[] classFields = field.getType().getDeclaredFields();
-                        boolean dbiIsSet = false;
-                        if (classFields != null) {
-                            for (Field classField : classFields) {
-                                if (classField.getType().equals(DBI.class)) {
-                                    classField.setAccessible(true);
-                                    classField.set(dbiDao, dbi);
-                                    dbiIsSet = true;
-                                    break;
-                                }
-                            }
-                        }
-                        if (!dbiIsSet) {
-                            throw new IllegalStateException("Unable find a field with type DBI");
-                        }
-                    } else {
-                        throw new IllegalStateException("Unable find a constructor for class '"
-                                + field.getDeclaringClass() + "'");
-                    }
-
-                    field.setAccessible(true);
-                    field.set(test, dbiDao);
+                    handleDbiHandle(test, field);
+                } else if (annotation.annotationType().equals(DBIInstance.class)) {
+                    handleDbiInstance(test, field);
+                } else if (annotation.annotationType().equals(TestedDBIDao.class)) {
+                    handleDbiDao(test, field);
                 }
             }
         }
+    }
+
+    private void handleDbiHandle(Object test, Field field) throws IllegalAccessException {
+        if (!field.getType().equals(Handle.class)) {
+            throw new IllegalArgumentException("Unable inject a DBI handle to a " +
+                    "field with type " + field.getType());
+        }
+        if (Modifier.isStatic(field.getModifiers())) {
+            throw new IllegalArgumentException("Unable inject a DBI Handle to a static field");
+        }
+        field.setAccessible(true);
+        field.set(test, handle);
+    }
+
+    private void handleDbiInstance(Object test, Field field) throws IllegalAccessException {
+        if (!field.getType().equals(DBI.class)) {
+            throw new IllegalArgumentException("Unable inject a DBI instance to " +
+                    "a field with type " + field.getType());
+        }
+        if (Modifier.isStatic(field.getModifiers())) {
+            throw new IllegalArgumentException("Unable inject a DBI instance to a static field");
+        }
+        field.setAccessible(true);
+        field.set(test, dbi);
+    }
+
+    private void handleDbiDao(Object test, Field field) throws IllegalAccessException {
+        if (Modifier.isStatic(field.getModifiers())) {
+            throw new IllegalArgumentException("Unable inject a DBI DAO to a static field");
+        }
+
+        Object dbiDao;
+
+        Constructor<?> defaultConstructor = null;
+        Constructor<?> constructorWithParameters = null;
+        Constructor<?>[] constructors = field.getType().getDeclaredConstructors();
+        for (Constructor<?> constructor : constructors) {
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+            if (parameterTypes.length == 1 && parameterTypes[0].equals(DBI.class)) {
+                constructorWithParameters = constructor;
+            } else if (parameterTypes.length == 0) {
+                defaultConstructor = constructor;
+            }
+        }
+        if (constructorWithParameters != null) {
+            constructorWithParameters.setAccessible(true);
+            try {
+                dbiDao = constructorWithParameters.newInstance(dbi);
+            } catch (Exception e) {
+                throw new RuntimeException("Unable to create an instance of class '"
+                        + field.getClass() + "'", e);
+            }
+        } else if (defaultConstructor != null) {
+            defaultConstructor.setAccessible(true);
+            try {
+                dbiDao = defaultConstructor.newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException("Unable to create an instance of class '"
+                        + field.getClass() + "'", e);
+            }
+            Field[] classFields = field.getType().getDeclaredFields();
+            boolean dbiIsSet = false;
+            if (classFields != null) {
+                for (Field classField : classFields) {
+                    if (classField.getType().equals(DBI.class)) {
+                        classField.setAccessible(true);
+                        classField.set(dbiDao, dbi);
+                        dbiIsSet = true;
+                        break;
+                    }
+                }
+            }
+            if (!dbiIsSet) {
+                throw new IllegalStateException("Unable find a field with type DBI");
+            }
+        } else {
+            throw new IllegalStateException("Unable find a constructor for class '"
+                    + field.getDeclaringClass() + "'");
+        }
+
+        field.setAccessible(true);
+        field.set(test, dbiDao);
     }
 
     @Override
