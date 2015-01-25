78787533483d075e0eebbc0ab0a7f6afb7d966cf
Artem Prigoda <arteamon@gmail.com>
2015-01-25 23:52:54 +0300

    Support for injecting DBI SQL objects to tests

---
+++ src/test/java/com/github/arteam/dropwizard/testing/jdbi/PlayerSqlObject.java
@@ -0,0 +1,28 @@
+package com.github.arteam.dropwizard.testing.jdbi;
+
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Date: 1/25/15
+ * Time: 11:26 PM
+ *
+ * @author Artem Prigoda
+ */
+public interface PlayerSqlObject {
+
+    @GetGeneratedKeys
+    @SqlUpdate("insert into players(first_name, last_name, birth_date, weight, height) values" +
+            "(:first_name, :last_name, :birth_date, :weight, :height)")
+    Long createPlayer(@Bind("first_name") String firstName, @Bind("last_name") String lastName,
+                      @Bind("birth_date") Date birthDate, @Bind("height") int height,
+                      @Bind("weight") int weight);
+
+    @SqlQuery("select last_name from players")
+    List<String> getLastNames();
+}
