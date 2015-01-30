bc5dbb07bc73af9475aff2f490abb22e604ad782
Artem Prigoda <aprigoda@bcc.ru>
2015-01-31 00:01:34 +0300

    Add more tests for DataSet annotation for the class level
    Also make birth date a data, not a timestamp

--- src/test/resource/playerDao/getAmountPlayersBornInYear.sql
+++ src/test/resource/playerDao/getAmountPlayersBornInYear.sql
@@ -1,8 +1,8 @@
 insert into players(first_name, last_name, birth_date, weight, height)
-values ('Vladimir','Tarasenko', '1991-08-05 00:00:00', 99, 184);
+values ('Vladimir','Tarasenko', '1991-08-05', 99, 184);
 insert into players(first_name, last_name, birth_date, weight, height)
-values ('Tyler','Seguin', '1992-01-30 00:00:00', 88, 185);
+values ('Tyler','Seguin', '1992-01-30', 88, 185);
 insert into players(first_name, last_name, birth_date, weight, height)
-values ('Ryan','Ellis', '1991-01-03 00:00:00', 79, 176);
+values ('Ryan','Ellis', '1991-01-03', 79, 176);
 insert into players(first_name, last_name, birth_date, weight, height)
-values ('John','Tavares', '1990-09-20 00:00:00', 93, 185);
+values ('John','Tavares', '1990-09-20', 93, 185);
--- src/test/resource/schema.sql
+++ src/test/resource/schema.sql
@@ -2,7 +2,7 @@ create table players(
     id bigint not null identity,
     first_name varchar(128) not null,
     last_name varchar(128) not null,
-    birth_date timestamp not null,
+    birth_date date not null,
     weight int null,
     height int null
 );
