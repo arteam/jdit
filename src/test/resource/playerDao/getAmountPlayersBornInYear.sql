53c71952e545ddd3fdcbb04248ba7e0e21520f4f
Artem Prigoda <arteamon@gmail.com>
2015-01-30 23:52:32 +0300

    Support for DataSet annotation on the class level

---
+++ src/test/resource/playerDao/getAmountPlayersBornInYear.sql
@@ -0,0 +1,8 @@
+insert into players(first_name, last_name, birth_date, weight, height)
+values ('Vladimir','Tarasenko', '1991-08-05 00:00:00', 99, 184);
+insert into players(first_name, last_name, birth_date, weight, height)
+values ('Tyler','Seguin', '1992-01-30 00:00:00', 88, 185);
+insert into players(first_name, last_name, birth_date, weight, height)
+values ('Ryan','Ellis', '1991-01-03 00:00:00', 79, 176);
+insert into players(first_name, last_name, birth_date, weight, height)
+values ('John','Tavares', '1990-09-20 00:00:00', 93, 185);
