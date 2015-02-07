a931be7f3718326ee65306d1bcf808926f500007
Artem Prigoda <arteamon@gmail.com>
2015-02-07 17:39:25 +0300

    Test for transactions and complex SQL objects

---
+++ src/test/java/com/github/arteam/dropwizard/testing/jdbi/domain/entity/Player.java
@@ -0,0 +1,48 @@
+package com.github.arteam.dropwizard.testing.jdbi.domain.entity;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Optional;
+
+import java.util.Date;
+
+/**
+ * Date: 2/7/15
+ * Time: 5:00 PM
+ *
+ * @author Artem Prigoda
+ */
+public class Player {
+
+    public final Optional<Long> id;
+    public final String firstName;
+    public final String lastName;
+    public final Date birthDate;
+    public final int height;
+    public final int weight;
+
+    public Player(Optional<Long> id, String firstName, String lastName, Date birthDate, int height, int weight) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public Player(String firstName, String lastName, Date birthDate, int height, int weight) {
+        this(Optional.<Long>absent(), firstName, lastName, birthDate, height, weight);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .omitNullValues()
+                .add("id", id.orNull())
+                .add("firstName", firstName)
+                .add("lastName", lastName)
+                .add("birthDate", birthDate)
+                .add("height", height)
+                .add("weight", weight)
+                .toString();
+    }
+}
