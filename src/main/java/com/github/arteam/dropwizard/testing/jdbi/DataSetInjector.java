c3492f595706710cd33752d5b0367aa2cef2581d
Artem Prigoda <arteamon@gmail.com>
2015-02-09 23:37:46 +0300

    Add documentation to DataSetInjector

--- src/main/java/com/github/arteam/dropwizard/testing/jdbi/DataSetInjector.java
+++ src/main/java/com/github/arteam/dropwizard/testing/jdbi/DataSetInjector.java
@@ -7,6 +7,11 @@ import java.lang.reflect.Method;
 /**
  * Date: 2/1/15
  * Time: 3:24 PM
+ * <p/>
+ * Component that responsible for injecting test data to methods.
+ * It analyzes test method and classes for {@link DataSet} annotation
+ * and dispatches locations of the data to {@link DataMigration}
+ * component.
  *
  * @author Artem Prigoda
  */
@@ -18,6 +23,15 @@ public class DataSetInjector {
         this.dataMigration = dataMigration;
     }
 
+    /**
+     * Inject test data to a method.
+     *
+     * If the method or class has {@link} DataSet annotation, data
+     * from script from the location specified in the annotation
+     * will be injected to the DB
+     *
+     * @param method current method
+     */
     public void injectData(Method method) {
         DataSet classLevelDataSet = method.getDeclaringClass().getAnnotation(DataSet.class);
         DataSet methodDataSet = method.getAnnotation(DataSet.class);
