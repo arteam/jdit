cbffc8b240acf349673cd37af7a3e28406c44cc7
Artem Prigoda <arteamon@gmail.com>
2015-02-16 22:58:37 +0300

    Ability to use custom DBI Factory

--- src/main/java/com/github/arteam/jdit/DBIContext.java
+++ src/main/java/com/github/arteam/jdit/DBIContext.java
@@ -1,19 +1,7 @@
 package com.github.arteam.jdit;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
-import io.dropwizard.jdbi.ImmutableListContainerFactory;
-import io.dropwizard.jdbi.ImmutableSetContainerFactory;
-import io.dropwizard.jdbi.NamePrependingStatementRewriter;
-import io.dropwizard.jdbi.OptionalContainerFactory;
-import io.dropwizard.jdbi.args.JodaDateTimeArgumentFactory;
-import io.dropwizard.jdbi.args.JodaDateTimeMapper;
-import io.dropwizard.jdbi.args.OptionalArgumentFactory;
-import io.dropwizard.jdbi.logging.LogbackLog;
-import org.skife.jdbi.v2.ColonPrefixNamedParamStatementRewriter;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -41,7 +29,6 @@ public class DBIContext {
     private static final String PROPERTIES_LOCATION = "/jdbi-testing.properties";
     private static final String DEFAULT_SCHEMA_LOCATION = "schema.sql";
 
-    private static final Logger LOG = (Logger) LoggerFactory.getLogger(DBI.class);
     private static final Holder INSTANCE = new Holder();
 
     /**
@@ -80,18 +67,13 @@ public class DBIContext {
          * @return a new {@link DBI} instance for performing database access
          */
         private DBI createDBI(Properties properties) {
-            DBI dbi = new DBI(properties.getProperty("db.url"), properties.getProperty("db.username"),
-                    properties.getProperty("db.password"));
-
-            dbi.setSQLLog(new LogbackLog(LOG, Level.INFO));
-            dbi.setStatementRewriter(new NamePrependingStatementRewriter(new ColonPrefixNamedParamStatementRewriter()));
-            dbi.registerArgumentFactory(new OptionalArgumentFactory(null));
-            dbi.registerContainerFactory(new ImmutableListContainerFactory());
-            dbi.registerContainerFactory(new ImmutableSetContainerFactory());
-            dbi.registerContainerFactory(new OptionalContainerFactory());
-            dbi.registerArgumentFactory(new JodaDateTimeArgumentFactory());
-            dbi.registerMapper(new JodaDateTimeMapper());
-            return dbi;
+            try {
+                DBIFactory dbiFactory = (DBIFactory) Class.forName(properties.getProperty("dbi.factory"))
+                        .newInstance();
+                return dbiFactory.createDBI(properties);
+            } catch (Exception e) {
+                throw new IllegalStateException("Unable instantiate DBI Factory", e);
+            }
         }
 
         /**
