12a3c003cea9b9d995fcce192387f1e0d4c54e94
Artem Prigoda <aprigoda@bcc.ru>
2016-01-01 19:39:59 +0300

    Extract database maintenance into a separate entity
    This allows to support different databases for testing based on
    user's choice.

--- src/main/java/com/github/arteam/jdit/DataMigration.java
+++ src/main/java/com/github/arteam/jdit/DataMigration.java
@@ -24,47 +24,4 @@ class DataMigration {
                 scriptLocation : scriptLocation.substring(1);
         handle.createScript(correctLocation).executeAsSeparateStatements();
     }
-
-    /**
-     * Sweep data from DB, but don't drop the schema.
-     * Also restart sequences, so tests can rely on their predictability
-     */
-    public void sweepData() {
-        handle.useTransaction(new TransactionConsumer() {
-            @Override
-            public void useTransaction(Handle h, TransactionStatus transactionStatus) throws Exception {
-                Query<String> tableNames = h.createQuery("select tablename from pg_tables " +
-                        "where tableowner = (select current_user) " +
-                        "and schemaname = 'public'")
-                        .mapTo(String.class);
-                Batch batch = h.createBatch();
-                for (String tableName : tableNames) {
-                    batch.add(String.format("truncate table \"%s\" cascade", tableName));
-                }
-                Query<String> sequenceNames = h.createQuery("select sequence_name from information_schema.sequences " +
-                        "where sequence_schema='public' " +
-                        "and sequence_catalog = (select current_catalog)")
-                        .mapTo(String.class);
-                for (String sequenceName : sequenceNames) {
-                    h.execute(String.format("alter sequence \"%s\" restart with 1", sequenceName));
-                }
-                batch.execute();
-            }
-        });
-    }
-
-    /**
-     * Drop all tables and sequences
-     */
-    public void dropTablesAndSequences() {
-        handle.useTransaction(new TransactionConsumer() {
-            @Override
-            public void useTransaction(Handle h, TransactionStatus transactionStatus) throws Exception {
-                String currentUser = h.createQuery("select current_user")
-                        .mapTo(String.class)
-                        .first();
-                h.execute(String.format("drop owned by \"%s\"", currentUser));
-            }
-        });
-    }
 }
---
+++ src/main/java/com/github/arteam/jdit/DatabaseMaintenance.java
@@ -0,0 +1,24 @@
+package com.github.arteam.jdit;
+
+/**
+ * Date: 1/1/16
+ * Time: 6:57 PM
+ * <p/>
+ * An interface for representing vendor-specific database
+ * maintenance operations
+ *
+ * @author Artem Prigoda
+ */
+public interface DatabaseMaintenance {
+
+    /**
+     * Drop all tables and sequences
+     */
+    void dropTablesAndSequences();
+
+    /**
+     * Sweep data from DB, but don't drop the schema.
+     * Also restart sequences, so tests can rely on their predictability
+     */
+    void sweepData();
+}
