d38ea2e07fb7d1b723dbaed478ef20694079a417
Artem Prigoda <arteamon@gmail.com>
2016-01-01 20:38:47 +0300

    Support for H2 database
    Add support for maintenance operations in H2 database.

---
+++ src/main/java/com/github/arteam/jdit/H2DatabaseMaintenance.java
@@ -0,0 +1,49 @@
+package com.github.arteam.jdit;
+
+import org.skife.jdbi.v2.*;
+
+/**
+ * Date: 1/1/16
+ * Time: 8:11 PM
+ * Maintenance operations against an H2 database
+ *
+ * @author Artem Prigoda
+ */
+public class H2DatabaseMaintenance implements DatabaseMaintenance {
+
+    private final Handle handle;
+
+    public H2DatabaseMaintenance(Handle handle) {
+        this.handle = handle;
+    }
+
+    @Override
+    public void dropTablesAndSequences() {
+        // NOOP
+    }
+
+    @Override
+    public void sweepData() {
+        handle.useTransaction(new TransactionConsumer() {
+            @Override
+            public void useTransaction(Handle h, TransactionStatus status) throws Exception {
+                Batch batch = h.createBatch();
+                batch.add("set referential_integrity false");
+
+                Query<String> tables = h.createQuery("show tables")
+                        .mapTo(String.class);
+                for (String table : tables) {
+                    batch.add(String.format("truncate table \"%s\"", table));
+                }
+
+                Query<String> sequenceNames = h.createQuery("select sequence_name from information_schema.sequences")
+                        .mapTo(String.class);
+                for (String sequenceName : sequenceNames) {
+                    batch.add(String.format("alter sequence \"%s\" restart with 1", sequenceName));
+                }
+                batch.add("set referential_integrity true");
+                batch.execute();
+            }
+        });
+    }
+}
