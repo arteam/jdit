4ee7abaa34290ccc8430635abc5ccbb0a6618c9c
Artem Prigoda <arteamon@gmail.com>
2016-05-25 19:36:43 +0200

    Remove unused imports

--- src/test/java/com/github/arteam/jdit/MySqlDbTest.java
+++ src/test/java/com/github/arteam/jdit/MySqlDbTest.java
@@ -1,22 +1,6 @@
 package com.github.arteam.jdit;
 
-import com.github.arteam.jdit.annotations.DBIHandle;
-import com.github.arteam.jdit.annotations.DataSet;
 import com.github.arteam.jdit.annotations.JditProperties;
-import com.github.arteam.jdit.annotations.TestedSqlObject;
-import com.github.arteam.jdit.domain.PlayerSqlObject;
-import com.google.common.collect.ImmutableList;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.ISODateTimeFormat;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.skife.jdbi.v2.Handle;
-
-import java.util.List;
-import java.util.Map;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Date: 1/1/16
