c3fe9fc1734ae0a14f62a71366b91f50ceeb0f71
Artem Prigoda <arteamon@gmail.com>
2016-05-25 19:36:43 +0200

    Remove unused imports

--- src/test/java/com/github/arteam/jdit/HsqlDbTest.java
+++ src/test/java/com/github/arteam/jdit/HsqlDbTest.java
@@ -1,17 +1,6 @@
 package com.github.arteam.jdit;
 
-import com.github.arteam.jdit.annotations.DataSet;
 import com.github.arteam.jdit.annotations.JditProperties;
-import com.github.arteam.jdit.annotations.TestedSqlObject;
-import com.github.arteam.jdit.domain.PlayerSqlObject;
-import com.google.common.collect.ImmutableList;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.ISODateTimeFormat;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.util.List;
 
 /**
  * Date: 1/1/16
