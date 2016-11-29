672c2905d9d81ab1b46cef0c9078eca9a9a7237d
Artem Prigoda <artem.prigoda@glispamedia.com>
2016-11-29 20:05:35 +0100

    Add tests for basic and naive name strategies

---
+++ src/test/java/com/github/arteam/jdbi3/strategies/BasicSqlNameStrategyTest.java
@@ -0,0 +1,24 @@
+package com.github.arteam.jdbi3.strategies;
+
+import com.github.arteam.jdbi3.AbstractStrategyTest;
+import com.github.arteam.jdbi3.InstrumentedTimingCollector;
+import org.jdbi.v3.core.ExtensionMethod;
+import org.junit.Test;
+
+import static com.codahale.metrics.MetricRegistry.name;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+public class BasicSqlNameStrategyTest extends AbstractStrategyTest {
+
+    private BasicSqlNameStrategy basicSqlNameStrategy = new BasicSqlNameStrategy();
+    private InstrumentedTimingCollector collector = new InstrumentedTimingCollector(registry, basicSqlNameStrategy);
+
+    @Test
+    public void producesMethodNameAsMetric() throws Exception {
+        when(ctx.getExtensionMethod()).thenReturn(new ExtensionMethod(getClass(), getClass().getMethod("updatesTimerForBasicSqlName")));
+        String name = basicSqlNameStrategy.getStatementName(ctx);
+        assertThat(name).isEqualTo(name(getClass(), "updatesTimerForBasicSqlName"));
+    }
+
+}
---
+++ src/test/java/com/github/arteam/jdbi3/strategies/NaiveNameStrategyTest.java
@@ -0,0 +1,24 @@
+package com.github.arteam.jdbi3.strategies;
+
+import com.github.arteam.jdbi3.AbstractStrategyTest;
+import com.github.arteam.jdbi3.InstrumentedTimingCollector;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NaiveNameStrategyTest extends AbstractStrategyTest {
+
+    private NaiveNameStrategy naiveNameStrategy = new NaiveNameStrategy();
+    private InstrumentedTimingCollector collector = new InstrumentedTimingCollector(registry, naiveNameStrategy);
+
+    @Test
+    public void producesSqlRawMetrics() throws Exception {
+        collector.collect(TimeUnit.SECONDS.toNanos(1), ctx);
+
+        String name = naiveNameStrategy.getStatementName(ctx);
+        assertThat(name).isEqualToIgnoringCase("sql.raw");
+    }
+
+}
