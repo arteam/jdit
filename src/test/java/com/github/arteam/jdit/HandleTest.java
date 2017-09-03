117d6bf0640dc6996aad120dce60b8a7fbef611b
Artem Prigoda <arteamon@gmail.com>
2017-09-03 20:09:01 +0200

    Upgrade to jdbi-3.0.0-beta2

--- src/test/java/com/github/arteam/jdit/HandleTest.java
+++ src/test/java/com/github/arteam/jdit/HandleTest.java
@@ -22,7 +22,7 @@ public class HandleTest {
 
     @Test
     public void testInsert() {
-        int amount = handle.insert("insert into players(first_name, last_name, birth_date, weight, height)" +
+        int amount = handle.execute("insert into players(first_name, last_name, birth_date, weight, height)" +
                 " values ('Vladimir','Tarasenko', '1991-08-05', 84, 99)");
         Assert.assertEquals(amount, 1);
 
