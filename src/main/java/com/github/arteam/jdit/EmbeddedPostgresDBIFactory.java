5e45510fc87c8a886605600de8503880cfcb08e0
Artem Prigoda <arteamon@gmail.com>
2021-11-06 15:56:50 +0100

    Use embedded Postgres via TestContainers

--- src/main/java/com/github/arteam/jdit/EmbeddedPostgresDBIFactory.java
+++ src/main/java/com/github/arteam/jdit/EmbeddedPostgresDBIFactory.java
@@ -1,38 +1,35 @@
 package com.github.arteam.jdit;
 
-import com.opentable.db.postgres.embedded.EmbeddedPostgres;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-import java.io.IOException;
 import java.util.Properties;
 
 /**
  * Factory for creating a dynamic Postgres DBI instance
  */
 public class EmbeddedPostgresDBIFactory implements DBIFactory {
-
     private static final String USER = "jdit";
     private static final String DATABASE = "jdit_test";
     private static final String PASSWORD = "test";
-    private static final EmbeddedPostgres embeddedPostgres;
+    private static final PostgreSQLContainer<?> postgresContainer;
 
     static {
-        try {
-            embeddedPostgres = EmbeddedPostgres.builder().start();
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
-        }
-        try (Handle handle = Jdbi.create(embeddedPostgres.getPostgresDatabase()).open();) {
+        postgresContainer = new PostgreSQLContainer<>("postgres:13.4");
+        postgresContainer.start();
+        try (Handle handle = Jdbi.create(postgresContainer.getJdbcUrl(), postgresContainer.getUsername(),
+                postgresContainer.getPassword()).open()) {
             handle.execute(String.format("create user %s with password '%s'", USER, PASSWORD));
             handle.execute(String.format("create database %s with owner %s", DATABASE, USER));
         }
+        Runtime.getRuntime().addShutdownHook(new Thread(postgresContainer::stop));
     }
 
     @Override
     public Jdbi createDBI(Properties properties) {
-        Jdbi dbi = Jdbi.create(String.format("jdbc:postgresql://127.0.0.1:%d/%s", embeddedPostgres.getPort(), DATABASE),
-                USER, PASSWORD);
+        Jdbi dbi = Jdbi.create(String.format("jdbc:postgresql://127.0.0.1:%d/%s",
+                postgresContainer.getMappedPort(5432), DATABASE), USER, PASSWORD);
         dbi.installPlugins();
         return dbi;
     }
